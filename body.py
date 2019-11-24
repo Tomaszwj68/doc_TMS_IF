f45b90c23eba95c9e90b7b094a92c2847454da2b
Tomaszwj68 <tomaszwj68@gmail.com>
2019-11-24 20:50:54 +0100

    Zmiany w body.py - user_decision jako osobna procedura, wstepna reorganizacja kodu pod testy

--- body.py
+++ body.py
@@ -18,33 +18,47 @@ def merge(conf):
     exit()
 
 
-# Wybor przez usera co bedzie generowane. Zaimplementowane narazie jedynie polowicznie 1.
-print("CIS-COS Generator ver. 1.0\n")
-print("""1 - Generacja pelnego opisu interfejsu CIS-COS\n
-        2 - Porownanie dwoch wersji CIS-COS (nie zaimplementowane)\n
-        3 - XML z logika dla MDC (nie zaimplementowane)""")
-decision = input("Wybierz opcje (podaj numer):")
+decision = 0
 
-if float(decision) == 1:
-    try:
-        ver = input("Podaj numer wersji (np. 13.0):")
-        prev = input("Podaj numer poprzedniej wersji (np. 14.0):")
-        assert (float(prev) >= float(ver)), "Numer poprzedniej wersji nie moze byc wiekszy niz obecnej!"
-        # przywoluje klase i obrabiam wejscia do systemu, narazie nie dziala porownanie wejsc
-        Doc_in = CsvRider(ver, prev, 'IN_CIS_COS.csv')
-        Doc_in.rider()
-        # przywuluje klase na wyjscia z systemu
-        Doc_out = CsvRider(ver, prev, 'OUT_CIS_COS.csv')
-        Doc_out.rider()
-        Doc_out.compare(ver, prev)
-        del Doc_in
-        del Doc_out
-        merge(config_full)
 
-    except OSError:
-        print("Dokument CIS-COS zablokowany - nie mozna wygenerowac nowego.\n Zamknij otwarty dokument pdf lub "
-              "sprawdz nazwy plikow CSV i pdf.")
-    except KeyboardInterrupt:
-        print("Generacja przerwana przez uzytkownika")
-#    except:
- #       print("Nieznany rodzaj bledu.")
+def user_decision():
+    # Wybor przez usera co bedzie generowane. Zaimplementowane narazie jedynie polowicznie 1.
+    print("CIS-COS Generator ver. 1.0\n")
+    print("""1 - Generacja pelnego opisu interfejsu CIS-COS\n
+            2 - Porownanie dwoch wersji CIS-COS (nie zaimplementowane)\n
+            3 - XML z logika dla MDC (nie zaimplementowane)""")
+    while True:
+        global decision
+        decision = input("Wybierz opcje (podaj numer):")
+        # akceptowalne tylko liczby z przedzialu
+        # tymczasowo blad w momencie podania litery
+        if all ([decision.isdigit(), float(decision) in [1,2,3]]): break
+        print ("Niewlasciwa opcja, sproboj ponownie")
+if __name__ == '__main__':
+    user_decision()
+
+    if float(decision) == 1:
+        try:
+            ver = input("Podaj numer wersji (np. 13.0):")
+            prev = input("Podaj numer poprzedniej wersji (np. 14.0):")
+            assert (float(prev) <= float(ver)), "Numer poprzedniej wersji nie moze byc wiekszy niz obecnej!"
+            # przywoluje klase i obrabiam wejscia do systemu, narazie nie dziala porownanie wejsc
+            Doc_in = CsvRider(ver, prev, 'IN_CIS_COS.csv')
+            Doc_in.rider()
+            # przywuluje klase na wyjscia z systemu
+            Doc_out = CsvRider(ver, prev, 'OUT_CIS_COS.csv')
+            Doc_out.rider()
+            Doc_out.compare(ver, prev)
+            del Doc_in
+            del Doc_out
+            merge(config_full)
+
+        except OSError:
+            print("Dokument CIS-COS zablokowany - nie mozna wygenerowac nowego.\n Zamknij otwarty dokument pdf lub "
+                  "sprawdz nazwy plikow CSV i pdf.")
+        except KeyboardInterrupt:
+            print("Generacja przerwana przez uzytkownika")
+    #    except:
+     #       print("Nieznany rodzaj bledu.")
+    else:
+        print("Opcja narazie nie zaimplementowana.")
